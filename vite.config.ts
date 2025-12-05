2c2488a457a863f0eeff79ffab7cedb28e8b7262
TJPan <939692356@qq.com>
2025-12-05 13:45:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/',
+      base: '/tree-1205/',
       server: {
         port: 3010,
         host: '0.0.0.0',
